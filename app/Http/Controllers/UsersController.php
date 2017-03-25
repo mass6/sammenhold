f3996085e9632e7e2827b555517d6d96a3f5c2a4
Sam Ciaramilaro <samcharmy@gmail.com>
2017-03-25 16:02:27 +0100

    Create user endpoint

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function store(Request $request)
+    {
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt('unity'),
+                'phone' => $request->phone,
+                'is_patient' => $request->isPatient,
+            ]);
+        } catch (\Exception $e)
+        {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
+        return response()->json(['data' => $user->toArray()], 201);
+
+        return response()->json($request->all());
+    }
+}
